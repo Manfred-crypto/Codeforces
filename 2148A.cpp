1e401cbc12a4605588e8974bdb7cf66fffd33dbc
Manfred-crypto <leungcy314@gmail.com>
2026-02-10 13:45:28 +0800

    Create 2148A.cpp

---
+++ 2148A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k;
+    cin>>k;
+    int n;double x;
+    for(int i=0;i<k;i++){
+        cin>>x>>n;
+        if(n%2==0){cout<<"0"<<endl;}else{cout<<x<<endl;}
+    }
+}
