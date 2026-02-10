6802a67ff5af63322141e79c5e85ea4fd512fed5
Manfred-crypto <leungcy314@gmail.com>
2026-02-10 22:33:50 +0800

    Create 0266A.cpp

---
+++ 0266A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k=0;
+    string s;
+    cin>>n>>s;
+    for(int i=0;i<n-1;i++){
+        if(s[i]==s[i+1])k++;
+    }
+    cout<<k;
+}
