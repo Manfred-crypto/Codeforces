61119efd96c094a6d10875ae64647649ea8b1252
Manfred-crypto <leungcy314@gmail.com>
2026-02-11 19:00:33 +0800

    Create 1742A.cpp

---
+++ 1742A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+   int a,b,c;
+   cin>>a>>b>>c;
+   if(a+b==c||b+c==a||c+a==b){cout<<"YES";}else cout<<"NO";
+}
+int main(){
+   int t;
+   cin>>t;
+   while(t--){solve();cout<<endl;}
+}
