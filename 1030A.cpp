b93447f694a80017670fe702df5679240e700b22
Manfred-crypto <leungcy314@gmail.com>
2026-02-11 18:42:44 +0800

    Create 1030A.cpp

---
+++ 1030A.cpp
@@ -0,0 +1,12 @@
+
+ #include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n,a;bool b=1;
+   cin>>n;
+   for(int i=0;i<n;i++){
+      cin>>a;      
+      if(a==1){b=0;break;}
+   }
+   if(b==1){cout<<"easy";}else{cout<<"hard";}
+}
