77777777176f1f797e14f03edd3274f9de7dbd8b
Manfred-crypto <leungcy314@gmail.com>
2026-02-11 18:59:24 +0800

    Create 0996A.cpp

---
+++ 0996A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+   cin>>n;
+   int ans=n/100;
+   n=n%100;
+   ans+=n/20;
+   n=n%20;
+   ans+=n/10;
+   n=n%10;
+   ans+=n/5;
+   n=n%5;
+   ans+=n;
+   cout<<ans;
+}
