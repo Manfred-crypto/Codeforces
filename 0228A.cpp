7777777f5c40879022dc50cb6a9180a8a335c348
Manfred-crypto <leungcy314@gmail.com>
2026-02-25 12:21:53 +0800

    Create 0228A.cpp

---
+++ 0228A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int a,b,c,d,ans=0;
+   cin>>a>>b>>c>>d;
+   if(a==b||a==c||a==d){ans++;}
+   if(b==c||b==d){ans++;}
+   if(c==d){ans++;}
+   //fuck who made this
+   cout<<ans;
+}
